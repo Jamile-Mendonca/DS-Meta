c74960905fc24eff2769168a8f98ba58fdd6dcdc
Jamile-Xavier <jamile.x.m@gmail.com>
2022-09-07 16:13:11 -0300

    final aula 1DSMeta

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,6 +1,19 @@
+import Header from "./componentes/Header"
+import NotificationButton from "./componentes/NotificationButton"
+import SalesCard from "./componentes/SalesCard"
+
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <>
+    <Header/>
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard/>
+          </div>
+          </section>
+          </main>
+    </>
   )
 }
 
---
+++ FrontEnd/src/componentes/NotificationButton/index.tsx
@@ -0,0 +1,16 @@
+import icon from "../../assets/vector.svg"
+
+import "./styles.css"
+//. mesma pasta, .. pasta anterior
+
+function NotificationButton() {
+  return (
+   
+    <div className="dsmeta-red-btn">
+                          <img src={icon} alt="Notificar" />
+                        </div>
+  
+  )
+}
+
+export default NotificationButton
